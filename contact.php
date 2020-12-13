37c6c1efaacde8d26bda29c1648cdf3990291a4e
akanksh1a <38255773+akanksh1a@users.noreply.github.com>
2020-12-13 12:28:05 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,79 @@
+<?php
+  include('layouts/head_nav.php')
+?>
+<body>
+  <?php
+   include('layouts/nav.php')
+?>
+  <section id="contact">
+    <br>
+    <div class="container border py-3">
+      <h4 class="text-center text-primary display-4">Contact us</h4>
+      <br>
+      <div class="row">
+        <div class="col-md-8">
+          <div class="row ">
+            <div class="col ">
+              <h6 class="text-muted text-right">Registered Office Address:</h6>
+            </div>
+            <div class="col">
+              <p class="text-left">Pizzeria Private Limited,<br>
+              Proident id qui., Non est do.<br> Id ullamco.
+              Lorem ipsum <br> labore nulla cupidatat <br>excepteur.Koramangala,<br>
+              xyz - 000000 <br>
+              India
+              </p>
+            </div>
+          </div>
+          <div class="row ">
+            <div class="col ">
+              <h6 class="text-muted text-right">Toll Free Number:</h6>
+            </div>
+            <div class="col">
+              <p class="text-left">
+                1800-5656-6551
+              </p>
+            </div>
+          </div>
+          <div class="row ">
+            <div class="col ">
+              <h6 class="text-muted text-right">Telephone Number:</h6>
+            </div>
+            <div class="col">
+              <p class="text-left">
+                45454-65956
+              </p>
+            </div>
+          </div>
+          <div class="row ">
+            <div class="col ">
+              <h6 class="text-muted text-right">Fax Number:</h6>
+            </div>
+            <div class="col">
+              <p class="text-left">
+                592-545-8748
+              </p>
+            </div>
+          </div>
+          <div class="row ">
+            <div class="col ">
+              <h6 class="text-muted text-right">Email Address:</h6>
+            </div>
+            <div class="col">
+              <p class="text-left">
+                contact@pizzeria.com
+              </p>
+            </div>
+          </div>
+      </div>
+      <div class="col-md-4">
+        <img src="Image/contact.png" class="img-fluid" alt="">
+      </div>
+      </div>
+    </div>
+  </section>
+
+  <br><br>
+   <?php
+   include('layouts/footer.php')
+ ?>
